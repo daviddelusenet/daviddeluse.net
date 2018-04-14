a98d766527a870ac5af79181d5e2b0dfeefa9a72
David de Lusenet <david.delusenet@random.studio>
2018-04-14 16:09:39 +0200

    Added Webpack configuration for production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,3 +1,23 @@
 const webpack = require('webpack');
-const path = require('path');
+const { resolve } = require('path');
+const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const baseConfig = require('./webpack.base');
 
+module.exports = () => {
+  return(
+    merge(baseConfig(), {
+      devtool: 'source-map',
+      optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            sourceMap: true,
+            uglifyOptions: {
+              ecma: 8
+            }
+          })
+        ]
+      }
+    })
+  )
+};
