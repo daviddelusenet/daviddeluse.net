bc063a9899beef2bf5dfefc4e14c846fddaa157b
David de Lusenet <david.delusenet@random.studio>
2018-06-23 19:46:28 +0200

    Added CleanWebpackPlugin setup

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,12 +1,14 @@
 const { resolve } = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { distPath } = require('./paths');
 
 module.exports = () => ({
   entry: {
     'daviddeluse.net': ['babel-polyfill', resolve(__dirname, './../src/index.js')],
   },
   output: {
-    path: resolve(__dirname, './../dist'),
+    path: distPath,
     filename: '[name].[hash].min.js',
   },
   module: {
@@ -25,6 +27,9 @@ module.exports = () => ({
     ],
   },
   plugins: [
+    new CleanWebpackPlugin([distPath], {
+      allowExternal: true,
+    }),
     new HtmlWebpackPlugin({
       template: resolve(__dirname, './../src/templates/index.html'),
     }),
