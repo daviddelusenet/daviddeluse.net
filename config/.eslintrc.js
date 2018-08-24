2d13a260ca34dc6e1ed6d0a479e005a0db82adae
David de Lusenet <david.delusenet@random.studio>
2018-08-24 09:49:27 +0200

    Updated ESLint configuration + babel-plugin-styled-components is now only used during development

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -8,17 +8,47 @@ module.exports = {
     jsx: true
   },
   plugins: [
-    'react'
+    'babel', 'react'
   ],
   extends: 'airbnb',
   rules: {
+    'babel/semi': 1,
+    'brace-style': ['error', '1tbs'],
+    curly: ['error', 'all'],
     'import/no-extraneous-dependencies': 0,
     'jsx-a11y/anchor-is-valid': ['error', {
       components: ['Link'],
       specialLink: ['to']
     }],
     'jsx-a11y/media-has-caption': 0,
+    'object-property-newline': ['error', {
+      allowAllPropertiesOnSameLine: false,
+    }],
     'react/jsx-filename-extension': 0,
+    'react/jsx-max-props-per-line': ['error', {
+      maximum: 3,
+    }],
+    'react/jsx-sort-default-props': ['error', {
+      ignoreCase: false,
+    }],
+    'react/jsx-sort-props': ['error', {
+      callbacksLast: false,
+      ignoreCase: false,
+      noSortAlphabetically: false,
+      reservedFirst: false,
+      shorthandFirst: false,
+      shorthandLast: false,
+    }],
+    'react/sort-prop-types': ['error', {
+      callbacksLast: false,
+      ignoreCase: false,
+      requiredFirst: false,
+      sortShapeProp: false,
+    }],
+    'sort-keys': ['warn', 'asc', {
+      caseSensitive: true,
+      natural: false,
+    }],
   },
   globals: {
   }
