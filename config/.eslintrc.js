6948f3607b3eb58d6ae539caaf70cb2fb75f865d
David de Lusenet <david.delusenet@random.studio>
2019-02-09 16:05:25 +0100

    Add padding-line-between-statements and sort-imports ESLint rules configuration

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'brace-style': ['error', '1tbs'],
     curly: ['error', 'all'],
     'import/no-extraneous-dependencies': 0,
+    'import/order': 0,
     'jsx-a11y/anchor-is-valid': ['error', {
       components: ['Link'],
       specialLink: ['to']
@@ -29,6 +30,18 @@ module.exports = {
     'object-property-newline': ['error', {
       allowAllPropertiesOnSameLine: false,
     }],
+    'padding-line-between-statements': ['error',
+      {
+        blankLine: 'always',
+        prev: ['case', 'multiline-block-like', 'multiline-const', 'multiline-expression', 'multiline-let', 'multiline-var'],
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['multiline-block-like', 'multiline-const', 'multiline-expression', 'multiline-let', 'multiline-var', 'return'],
+      },
+    ],
     'react/jsx-filename-extension': 0,
     'react/jsx-max-props-per-line': ['error', {
       maximum: 3,
@@ -52,6 +65,11 @@ module.exports = {
       ignoreCase: false,
       requiredFirst: false,
       sortShapeProp: false,
+    }],
+    'sort-imports': ['error', {
+      ignoreCase: true,
+      ignoreMemberSort: false,
+      memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
     }],
     'sort-keys': ['warn', 'asc', {
       caseSensitive: true,
