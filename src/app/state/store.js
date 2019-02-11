fbce70ecdb31be3b80a2ec262c9a9585080bc36d
David de Lusenet <david.delusenet@random.studio>
2019-02-11 20:16:11 +0100

    Implement redux-logger

--- src/app/state/store.js
+++ src/app/state/store.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import mainReducer from './reducers/mainReducer';
 
-export default createStore(mainReducer);
+export default createStore(
+  mainReducer,
+  applyMiddleware(logger),
+);
